3edf3bc6c441a44d891be2f6a6f27d670e85d361
Reza Moallemi <rm.allmails@gmail.com>
2020-03-22 13:33:40 +0100

    Add unit tests

---
+++ src/test/kotlin/me/moallemi/tools/daterange/localdate/LocalDateRangeTest.kt
@@ -0,0 +1,41 @@
+package me.moallemi.tools.daterange.localdate
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import java.time.LocalDate
+
+
+class LocalDateRangeTest {
+
+    @Test
+    fun testRange() {
+        val expected = listOf(
+            "2020-01-01",
+            "2020-01-02",
+            "2020-01-03",
+            "2020-01-04",
+            "2020-01-05"
+        )
+        val startDate = LocalDate.of(2020, 1, 1)
+        val endDate = LocalDate.of(2020, 1, 5)
+
+        val actual = (startDate..endDate).iterator().asSequence().toList().map { it.toString() }
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testStep() {
+        val expected = listOf(
+            "2020-01-01",
+            "2020-01-03",
+            "2020-01-05"
+        )
+        val startDate = LocalDate.of(2020, 1, 1)
+        val endDate = LocalDate.of(2020, 1, 5)
+
+        val actual = (startDate..endDate step 2).iterator().asSequence().toList().map { it.toString() }
+
+        assertEquals(expected, actual)
+    }
+}
